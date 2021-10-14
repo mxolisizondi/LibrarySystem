549b507bedb74b6cb623f00bafc26392efbebd62
Mxolisi Zondi <mxolisizondi20@gmail.com>
2021-10-14 16:19:01 +0200

    creating models

---
+++ LibrarySystem/Models/StaffsModel.cs
@@ -0,0 +1,41 @@
+﻿using LibrarySystem.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Models
+{
+    class StaffsModel
+    {
+        public int StaffID { get; set; }
+        public int DesignationID { get; set; }
+        public string Name { get; set; }
+        public string TCNO { get; set; }
+        public int Gender { get; set; }
+        public string Address { get; set; }
+        public string ContactNo { get; set; }
+
+        public Designations Designations = new Designations();
+
+        public string _Gender
+        {
+            get
+            {
+                if (Gender == 0)
+                {
+                    return "Male";
+                }
+                else if (Gender == 1)
+                {
+                    return "Female";
+                }
+                else
+                {
+                    return "Other";
+                }
+            }
+        }
+    }
+}
